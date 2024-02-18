12390de5218677aa8536d6aa3026dff9564f8561
Andrew <49104171+carsonak@users.noreply.github.com>
2024-02-19 01:46:39 +0300

    Insertion?

--- sort.h
+++ sort.h
@@ -21,5 +21,8 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+int insert_after(listint_t **dest, listint_t **src);
+int insert_top(listint_t **head, listint_t **src);
 
 #endif /*_SORT_H_*/
