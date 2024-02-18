631be4d6e2322b8814fe1eb2abfbe0a1ce7c6137
Andrew <49104171+carsonak@users.noreply.github.com>
2024-02-18 22:09:12 +0300

    Added bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,10 +1,29 @@
 #include "sort.h"
 
 /**
- * bubble_sort - meh
- * @array: e
- * @size: e
+ * bubble_sort - bubble sort pushes the largest element to the end of the array
+ * @array: array to be sorted
+ * @size: length of the array
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t g = 1, max = size, new_max = 0;
+	int b = 0;
+
+	while (max > 1)
+	{
+		for (g = 1; g < max; g++)
+		{
+			if (array[g - 1] > array[g])
+			{
+				b = array[g - 1];
+				array[g - 1] = array[g];
+				array[g] = b;
+				new_max = g;
+				print_array(array, size);
+			}
+		}
+
+		max = new_max;
+	}
 }
--- 0-main.c
+++ 0-main.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+	int arr2[] = {1, 2, 3, 4, 5, 6, 7, 8};
 	size_t n = sizeof(array) / sizeof(array[0]);
 
 	print_array(array, n);
@@ -15,5 +16,11 @@ int main(void)
 	bubble_sort(array, n);
 	printf("\n");
 	print_array(array, n);
+
+	print_array(arr2, (sizeof(arr2) / sizeof(arr2[0])));
+	printf("\n");
+	bubble_sort(arr2, sizeof(arr2) / sizeof(arr2[0]));
+	printf("\n");
+	print_array(arr2, sizeof(arr2) / sizeof(arr2[0]));
 	return (0);
 }
