3a10fe9af025451714a8e27aa62871f0cf5acc97
Andrew <49104171+carsonak@users.noreply.github.com>
2024-02-20 11:09:29 +0300

    fixed quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -24,7 +24,7 @@ void range_manager(int *array, size_t bot, size_t top, size_t size)
 	size_t piv_i = 0;
 
 	/*Base case: partition should have atleast 2 elements*/
-	if (!array || !size || (top - bot) < 2)
+	if (!array || !size || top > size || (top - bot) < 2)
 		return;
 
 	piv_i = lomuto_partition(array, bot, top, size);
@@ -51,27 +51,30 @@ void range_manager(int *array, size_t bot, size_t top, size_t size)
  */
 size_t lomuto_partition(int *array, size_t bot, size_t top, size_t size)
 {
-	size_t g = bot + 1, piv_i = bot;
+	/*g searches the upper boundary*/
+	size_t g = bot + 1;
+	/*piv_i keeps track of the border between the boundaries*/
+	size_t piv_i = bot;
 	int pivot = array[top - 1], tmp = 0;
 
-	if (!array || !size)
+	if (!array || !size || top > size)
 		return (0);
 
 	while (g < top - 1)
 	{
-		/*Push items less than or equal to the pivot to the left of piv_i*/
-		if (array[g] <= pivot)
+		/*Swapping items less than pivot to the lower boundary*/
+		/*Swapping will only occur if necessary*/
+		if (array[g] <= pivot && array[piv_i] > pivot)
 		{
-			if (array[piv_i] > pivot)
-			{
-				tmp = array[g];
-				array[g] = array[piv_i];
-				array[piv_i] = tmp;
-				print_array(array, size);
-			}
-
-			piv_i++;
+			tmp = array[g];
+			array[g] = array[piv_i];
+			array[piv_i] = tmp;
+			print_array(array, size);
 		}
+
+		/*Ensure a items <= pivot are always in the lower boundary*/
+		if (array[piv_i] <= pivot)
+			piv_i++;
 
 		g++;
 	}
